9949451daaa3adf404b3e126b8f598698df7c881
3nergetik <75131588+3nergetik@users.noreply.github.com>
2023-05-24 09:03:58 +0500

    Добавлено описание

--- task_final/solution.js
+++ task_final/solution.js
@@ -1,4 +1,6 @@
 function sendRequest(name, phone, address, goods, sum) {
+    
+    // Объявляем сам объект data и прописываем ему свойства, согласно заданию.
     let data = {client: `${name} ${phone}`,
                 order: {
                     address: `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`,
@@ -6,10 +8,14 @@ function sendRequest(name, phone, address, goods, sum) {
                 }, 
                 goods: []};
     
+    // Проходимся по массиву goods, тк нам нужно изменить в нем данные 
     for (let i = 0; i < goods.length; i++) {
+        
+        // Добавляем в свойство goods объекта data другой объект со свойствами title и count 
         data.goods.push({title: goods[i].title, count: goods[i].count});
     };
-
+    
+    // Задаем в объекте data свойство data (так нужно по заданию)
     let jsonData = JSON.stringify({data: data});
     return jsonData;
 };
